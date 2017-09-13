8e01eb1486400873485dedcd58a2d9d44abce2f4
Geert Barentsen <geert@barentsen.be>
2017-09-12 21:36:32 -0700

    Extra commit for @mirca to trigger Travis

--- octopus/tests/test_genericness.py
+++ octopus/tests/test_genericness.py
@@ -4,5 +4,5 @@ from .. import models
 
 def test_whether_octopus_is_generic():
     for bad_word in ["Prior", "Likelihood"]:
-        assert(bad_word not in dir(core))
         assert(bad_word not in dir(models))
+        assert(bad_word not in dir(core))
